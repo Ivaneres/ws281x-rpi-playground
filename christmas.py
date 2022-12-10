69be95982897651bdc87fee05aa5627ff1496b1e
Ivan Ereshchenko <ereshchenkoi@gmail.com>
2022-12-10 23:02:19 +0000

    added christmas :)

---
+++ christmas.py
@@ -0,0 +1,44 @@
+import socket
+import pickle
+from tracemalloc import start
+from turtle import color
+from rpi_ws281x import PixelStrip, Color
+import queue
+import math
+import schedule
+import time
+from datetime import datetime
+
+LED_COUNT = 288       # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+PATTERN_STEP = 10
+PATTERN_COLORS = ((255, 0, 0), (0, 255, 0))
+
+def switch_on():
+    color_counter = 0
+    for i in range(LED_COUNT):
+        if (i % PATTERN_STEP) == 0:
+            color_counter += 1
+        if color_counter >= len(PATTERN_COLORS):
+            color_counter = 0
+        strip.setPixelColor(i, Color(*PATTERN_COLORS[color_counter]))
+
+def switch_off():
+    for i in range(LED_COUNT):
+        strip.setPixelColor(i, Color(0, 0, 0))
+        
+if datetime.now().time() > datetime.datetime(1970, 1, 1, 16, 30).time():
+    switch_on()
+
+schedule.every().day.at("16:30").do(switch_on)
+schedule.every().day.at("00:00").do(switch_off)
